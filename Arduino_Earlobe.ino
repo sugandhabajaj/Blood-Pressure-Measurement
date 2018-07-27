ed5ba82de3c3ca8613b216a4d30c4096f1fd419a
Sugandha Bajaj <37800507+sugandhabajaj@users.noreply.github.com>
2018-07-27 11:50:52 +0530

    Add files via upload

---
+++ Arduino_Earlobe.ino
@@ -0,0 +1,12 @@
+int sensor_val;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensor_val =analogRead(A0);
+  Serial.println(sensor_val);
+  delay(1);
+}
